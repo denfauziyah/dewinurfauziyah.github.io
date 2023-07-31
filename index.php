2af89b5b4324d4bbeafefe5180036c56bb20776b
Dewi Nur Fauziyah <76090710+denfauziyah@users.noreply.github.com>
2023-07-31 16:19:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Analisis Sentimen</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@1,300&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="gaya.css">
+</head>
+<body>
+	<div class="container">
+		<!-- INI SIDEBAR -->
+		<div class="sidebar">
+			<div class="header">
+				<div class="list-item">
+					<a href="#">
+						<img src="assets/.svg" alt="" class="icon">
+						<span class="description-header">Analisis Sentimen</span>
+					</a>
+				</div>
+				<div class="illustration">
+					<img src="assets/illustrationn.png" width =200px alt="">
+				</div>
+			</div>
+			<div class="main">
+				<div class="list-item">
+					<a href="#">
+						<img src="assets/dashboard2.svg" width = 18px alt="" class="icon">
+						<span class="description">Dashboard</span>
+					</a>
+				</div>
+				<div class="list-item">
+					<a href="#">
+						<img src="assets/data3.svg" width = 18px alt="" class="icon">
+						<span class="description">Data Training</span>
+					</a>
+				</div>
+				<div class="list-item">
+					<a href="#">
+						<img src="assets/data1.svg" width = 18px alt="" class="icon">
+						<span class="description">Data Kata Dasar</span>
+					</a>
+				</div>
+				<div class="list-item">
+					<a href="#">
+						<img src="assets/data2.svg" width = 18px alt="" class="icon">
+						<span class="description">Data Stopwords</span>
+					</a>
+				</div>
+				<div class="list-item">
+					<a href="#">
+						<img src="assets/analisis1.svg" width = 18px alt="" class="icon">
+						<span class="description">Halaman Analisis</span>
+					</a>
+				</div>
+				<div class="list-item">
+					<a href="#">
+						<img src="assets/grafik1.svg" width = 18px alt="" class="icon">
+						<span class="description">Grafik Sentimen</span>
+					</a>
+				</div>
+			</div>	
+		</div>
+		<div class="main-content">
+			<div id="menu-button">
+				<input type="checkbox" id="menu-checkbox">
+				<label for="menu-checkbox" id="menu-label">
+					<div id="hamburger"></div>
+				</label>
+			</div>
+			
+		</div>
+		
+	</div>
+
+
+
+	<script src="sekrip.js"></script>
+</body>
+</html>
